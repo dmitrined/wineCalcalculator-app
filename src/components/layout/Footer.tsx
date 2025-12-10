e9621b7b51e465a6b97c7f64c485d54a47e0ad78
dmitrined <dm.nedioglo@gmail.com>
2025-12-10 21:10:18 +0100

    ok

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { useAppSelector } from '../../app/hooks';
 import { translations } from '../../language/translations';
 
